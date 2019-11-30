4e2cae6bd19cd19a02e0c096efb6af088549c90a
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-30 10:20:26 +0100

    Add atomic instructions

---
+++ src/cmpxchg.h
@@ -0,0 +1,42 @@
+#ifndef CMPXCHG_H
+#define CMPXCHG_H
+
+#include <stdint.h>
+
+#if defined i386 || defined __i386__ || defined __x86_64__
+
+#define CMPXCHG32_ASM(addr, oldv, newv)                     \
+    asm volatile ("lock; cmpxchg %2, %1"                    \
+                  : "=a"((oldv))                            \
+                  : "m"(*(addr)), "r"((newv)), "a"((oldv))  \
+                  : "memory" )
+
+static inline int32_t cmpxchg32(int32_t *addr, int32_t oldv, int32_t newv) {
+    CMPXCHG32_ASM(addr, oldv, newv);
+    return oldv;
+}
+
+static inline uint32_t cmpxchgu32(uint32_t *addr, uint32_t oldv, uint32_t newv) {
+    CMPXCHG32_ASM(addr, oldv, newv);
+    return oldv;
+}
+
+static inline void atomic_write32(int32_t *addr, uint32_t v) {
+    int32_t old;
+    do {
+        old = *addr;
+    } while(cmpxchg32(addr, old, v) != old);
+}
+
+static inline void atomic_writeu32(uint32_t *addr, uint32_t v) {
+    uint32_t old;
+    do {
+        old = *addr;
+    } while(cmpxchgu32(addr, old, v) != old);
+}
+
+#else
+#error Hardware cmpxchg not available
+#endif
+
+#endif
