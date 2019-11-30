02c4d8968b97980601aecb1dce603bd87e49be67
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-30 12:29:14 +0100

    Add complex utilities

---
+++ src/complexpt.c
@@ -0,0 +1,23 @@
+#include "complexpt.h"
+
+#include <math.h>
+
+float complexptf_dist(struct complexptf const *p1, struct complexptf const *p2) {
+    float d_re = p1->re - p2->re;
+    float d_im = p1->im - p2->im;
+    return sqrtf(d_re * d_re + d_im * d_im);
+}
+
+float complexptf_magnitude(struct complexptf const *p) {
+    return sqrtf(p->re * p->re + p->im * p->im);
+}
+
+float complexptf_dot(struct complexptf const *p1, struct complexptf const *p2) {
+    return p1->re * p2->re + p1->im * p2->im;
+}
+
+void complexptf_normalize(struct complexptf *p) {
+    float magnitude = complexptf_magnitude(p);
+    p->re /= magnitude;
+    p->im /= magnitude;
+}
---
+++ src/complexpt.h
@@ -0,0 +1,19 @@
+#ifndef COMPLEXPT_H
+#define COMPLEXPT_H
+
+#include <stdint.h>
+
+struct complexpti {
+    int32_t re, im;
+};
+
+struct complexptf {
+    float re, im;
+};
+
+float complexptf_dist(struct complexptf const *p1, struct complexptf const *p2);
+float complexptf_magnitude(struct complexptf const *p);
+float complexptf_dot(struct complexptf const *p1, struct complexptf const *p2);
+void complexptf_normalize(struct complexptf *p);
+
+#endif
